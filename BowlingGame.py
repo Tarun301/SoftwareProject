0eb32a74af9dbfd3a655fbb70cc853d3fb28a3ba
Tarun301 <65336900+Tarun301@users.noreply.github.com>
2021-09-20 14:04:00 +1200

    Commented the functions and methods

--- BowlingGame.py
+++ BowlingGame.py
@@ -2,9 +2,11 @@ class BowlingGame:
     def __init__(self):
         self.rolls = []
 
+    # This method checks the Number of rolls and pins knocked down by a player
     def roll(self, pins):
         self.rolls.append(pins)
 
+    # This method checks the score as per the frame Index.
     def score(self):
         result = 0
         rollIndex = 0
@@ -20,18 +22,24 @@ class BowlingGame:
                 rollIndex += 2
         return result
 
+    # This method checks the game as per the pins knocked down by a player is that a strike game.
     def isStrike(self, rollIndex):
         return self.rolls[rollIndex] == 10
 
+    # This method checks the game with the no of pins kocked down by the rolls as per the frame Index to check
+    # is that a Spare Game.
     def isSpare(self, rollIndex):
         return self.rolls[rollIndex] + self.rolls[rollIndex+1] == 10
 
+    # This method checks the Strike Game Score
     def strikeScore(self, rollIndex):
         return 10 + self.rolls[rollIndex+1] + self.rolls[rollIndex+2]
 
+    # This method checks the Spare Game Score
     def spareScore(self, rollIndex):
         return 10 + self.rolls[rollIndex+2]
 
+    # This method checks the Frame Score
     def frameScore(self, rollIndex):
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
 
--- Main.py
+++ Main.py
@@ -4,9 +4,12 @@ import BowlingGame
 
 class TestBowlingGame(unittest.TestCase):
 
+    # This is the Testing function of the Bowling Game that tests the test cases as self.
     def setUp(self):
         self.game = BowlingGame.BowlingGame()
 
+    # This is the testing function of the Gutter Game, When a player rolls the ball & no pins are knocked down
+    # That results in a score of zero, is called a Gutter Game.
     def testGutterGame(self):
         self.rollMany(0, 20)
         self.assertEqual(0, self.game.score())
@@ -15,6 +18,7 @@ class TestBowlingGame(unittest.TestCase):
         self.rollMany(1, 20)
         self.assertEqual(20, self.game.score())
 
+    # This is the testing function of one spare game roll, when a player got a spare in a frame Index.
     def testOneSpare(self):
         self.rollSpare()
         self.game.roll(3)
@@ -36,6 +40,7 @@ class TestBowlingGame(unittest.TestCase):
         self.rollMany(0, 18)
         self.assertEqual(67, self.game.score())
 
+    # This is testing function of a one strike game played by a player.
     def testOneStrike(self):
         self.game.roll(10)
         self.game.roll(4)
@@ -43,10 +48,15 @@ class TestBowlingGame(unittest.TestCase):
         self.rollMany(0, 16)
         self.assertEqual(24, self.game.score())
 
+    # If a player knocks down all 10 pins in the tenth frame, the player is allowed to throw 3 balls for that frame.
+    # This allows for a potential of 12 strikes in a single game
+    # and A maximum score of 300 points, is called a Perfect Game.
     def testPerfectGame(self):
         self.rollMany(10, 12)
         self.assertEqual(300, self.game.score())
 
+    # This function checks here is all the spare games played by the players
+    # When a player knocks down all the ten pins with the two balls of a frame then it is called a Spare
     def testAllSpares(self):
         self.rollMany(5, 21)
         assert self.game.score() == 150
@@ -57,14 +67,17 @@ class TestBowlingGame(unittest.TestCase):
             self.game.roll(pins)
         self.assertEqual(133, self.game.score())
 
+    # When a player knocks down all the ten pins with their first ball, then it results in a Strike.
     def testAllStrikes(self):
         self.rollMany(6, 31)
-        self.assertEquals(120, self.game.score())
+        self.assertEqual(120, self.game.score())
 
+    # This is the testing function for of rolling the many balls by a player.
     def rollMany(self, pins, num):
         for i in range(num):
             self.game.roll(pins)
 
+    # This function checks the spare balls by a player in the frames.
     def rollSpare(self):
         self.game.roll(5)
         self.game.roll(5)
